d88064733183b38cee67cccf3ac09100d75c1bfc
Vedansh_yadav <83397824+Official-Vedansh-Yadav@users.noreply.github.com>
2021-08-01 14:42:43 +0530

    Add files via upload

---
+++ components.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click',()=>{
+    document.querySelector('.navlist').classList.toggle('hide-navlist');
+    document.querySelector('.navbar').classList.toggle('height-navbar');
+});
